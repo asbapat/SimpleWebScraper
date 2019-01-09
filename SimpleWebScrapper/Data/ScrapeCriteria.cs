4667ec6bcc842c25916d6f9ec92732efee49b183
Aniket Bapat <aniket.bapat@eng.sita.aero>
2019-01-09 16:13:58 -0500

    added ScrapeCriteria

--- SimpleWebScrapper/Data/ScrapeCriteria.cs
+++ SimpleWebScrapper/Data/ScrapeCriteria.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SimpleWebScrapper.Data
 {
@@ -10,9 +11,12 @@ namespace SimpleWebScrapper.Data
     {
         public ScrapeCriteria()
         {
-
+            Parts = new List<ScrapeCriteriaPart>(0);
         }
+        public string Data { get; set; }
+        public string Regex { get; set; }
+        public RegexOptions RegexOptions { get; set; }
+        public List<ScrapeCriteriaPart> Parts { get; set; }
 
-       
     }
 }
